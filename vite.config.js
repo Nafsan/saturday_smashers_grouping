e92623c722c85db63772b165f70b567ffdd5e423
Md. Shafiqul Islam Nafsan <iamnafsan@gmail.com>
2026-03-08 17:37:11 +0600

    Feature: Update vite config to allow vercel url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/saturday_smashers_grouping/', // IMPORTANT: Replace with your repository name
+    base: process.env.VERCEL ? '/' : '/saturday_smashers_grouping/', // IMPORTANT: Replace with your repository name
 })

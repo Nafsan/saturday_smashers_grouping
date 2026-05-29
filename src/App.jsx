0689ea43e954071b1ed247b33756cb9f7403b0ed
Md. Shafiqul Islam Nafsan <iamnafsan@gmail.com>
2026-05-29 11:19:51 +0600

    Feature: Remove iOS app install overlay

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,6 @@ import { ToastProvider } from './context/ToastContext';
 import { Analytics } from '@vercel/analytics/react';
 import './styles/global.scss';
 import Footer from './components/Footer';
-import IOSInstallPrompt from './components/IOSInstallPrompt';
-import './components/IOSInstallPrompt.scss';
-
 // Create custom themes matching the global SCSS variables
 const lightTheme = createTheme({
     palette: {
@@ -188,7 +185,6 @@ function AppWithTheme({ basename }) {
         <ThemeProvider theme={currentTheme}>
             <CssBaseline />
             <ToastProvider>
-                <IOSInstallPrompt />
                 <HashRouter>
                     <div className="page-wrapper">
                         <div className="main-content">
